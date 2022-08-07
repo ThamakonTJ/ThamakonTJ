bf56db688a311c3ce7342ab2b37e55eb2baa8e36
ThamakonTJ <thamakon.tj@gmail.com>
2022-08-07 23:12:35 +0700

    pr-update-extedform

--- resources/views/content/purchaing_requestion.blade.php
+++ resources/views/content/purchaing_requestion.blade.php
@@ -1,8 +1,56 @@
 @extends('master')
 @section('title', 'Purchaing Requestion PCM')
 
+
 @section('content')
- <h1>Purchaing Requestion</h1>
- <p>PR</p>
+
+    <h1>Purchaing Requestion</h1>
+    <form>
+        <div class="mb-3 row">
+            <label for="name" class="col-sm-1 col-form-label">ชื่อผู้จัดซื้อ</label>
+            <div class="col-sm-2">
+                <input type="text" class="form-control" id="name">
+            </div>
+            <label for="department" class="col-sm-1 col-form-label">แผนก</label>
+            <div class="col">
+                <input type="text" class="form-control" id="department">
+            </div>
+            <label for="date" class="col-sm-1 col-form-label">วันที่</label>
+            <div class="col-sm-2">
+                <input type="date" class="form-control" id="date">
+            </div>
+        </div>
+
+
+        <div class="mb-3 row">
+            <label for="reason_to_buy" class="col-form-label">เหตุผลในการขออนุมัติการจัดซื้อ</label>
+            <div class="col">
+                <input type="text" class="form-control" id="reason_to_buy">
+            </div>
+        </div>
+    </form>
+    <div class="container">
+        <form method="post" id="insert_form">
+         <div class="table-repsonsive">
+          <span id="error"></span>
+          <table class="table table-bordered" id="item_table">
+           <tr>
+            <th>รายการ</th>
+            <th>จำนวน</th>
+            <th>ราคาต่อหน่วย</th>
+            <th>ราคา</th>
+            <th>หมายเหตุ</th>
+            <th>รูปของสินค้า</th>
+            <th><button type="button" name="add" class="btn btn-success btn add"><span class="glyphicon glyphicon-plus"></span>+</button></th>
+           </tr>
+          </table>
+          <div align="center">
+           <input type="submit" name="submit" class="btn btn-success" value="submit" />
+          </div>
+         </div>
+        </form>
+       </div>
+ 
 
 @endsection
+
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContentController;
 use App\Models\Content;
+use Illuminate\Routing\Controller;
 use Illuminate\Routing\Route as RoutingRoute;
 
 /*
@@ -29,13 +30,23 @@ Route::middleware(['auth.admin'])-> group (function(){
     Route::get('/content', [ContentController::class,'index']);
     Route::get('/content/create', [ContentController::class, 'create']);
     Route::get('/content/{id}/edit', [ContentController::class, 'edit']);
+
+
+    ///ahp
     Route::get('/content/ahp', [ContentController::class, 'algorithm']);
+    ///PR
     Route::get('/content/purchaing_requestion', [ContentController::class, 'pr']);
+    ///QUO
     Route::get('/content/quotation', [ContentController::class, 'quo']);
+    ///PO
     Route::get('/content/purchaing_order', [ContentController::class, 'po']);
+    ///PI
     Route::get('/content/proforma_invoive', [ContentController::class, 'pi']);
+    ///INV
     Route::get('/content/invoive', [ContentController::class, 'inv']);
+    ///sup
     Route::get('/content/supplier', [ContentController::class, 'sup']);
+
 
     Route::post('/content', [ContentController::class,'store']);
     Route::put('/content/{id}', [ContentController::class,'update']);
